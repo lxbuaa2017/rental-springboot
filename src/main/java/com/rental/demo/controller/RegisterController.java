23ebfe0b85382c70cc13b60481edfa90168c213f
lxbuaa2017 <1753312844@qq.com>
2019-08-28 14:52:02 +0800

    bug fix

--- src/main/java/com/rental/demo/controller/RegisterController.java
+++ src/main/java/com/rental/demo/controller/RegisterController.java
@@ -12,11 +12,11 @@ import java.util.Map;
 @RestController
 @RequestMapping("/")
 public class RegisterController {
-    @Autowired
-    private UserService userService;
+//    @Autowired
+//    private UserService userService;
     @Autowired
     private TenantRegisterService tenantRegisterService;
-    @CrossOrigin//(origins = "http://114.115.160.38:8080", allowCredentials = "true")
+    @CrossOrigin(origins = "http://114.115.160.38:8080", allowCredentials = "true")
     @RequestMapping(value = "/register",method = RequestMethod.POST)
     @ResponseBody
     public int register(@RequestBody Map<String,Object> map){

f4fb1aef6d45ab9c2ad244ba6c840f8de96bf041
lxbuaa2017 <1753312844@qq.com>
2019-08-31 17:40:57 +0800

    合同好了

--- src/main/java/com/rental/demo/controller/PdfContractController.java
+++ src/main/java/com/rental/demo/controller/PdfContractController.java
@@ -11,6 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.w3c.dom.DocumentType;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -23,25 +24,39 @@ public class PdfContractController {
     private LongRentOrderRepository longRentOrderRepository;
     @Autowired
     private PdfFileRepository pdfFileRepository;
-    @GetMapping(value = "file/pdfContract",produces = {MediaType.APPLICATION_PDF_VALUE})
+    @GetMapping(value = "/file/pdfContract",produces = {MediaType.APPLICATION_PDF_VALUE})
     @ResponseBody
     public byte[] makePdf(@RequestParam String orderid){
         LongRentOrder longRentOrder=longRentOrderRepository.findById(orderid).get();
         byte[] data=null;
         PdfFile pdfFile=new PdfFile();
         pdfFile.setName(longRentOrder.getTenantName()+"的合同");
-        pdfFile.setLandLordName("青年房地产有限公司");
+        pdfFile.setLandLordName("青年房地产");
         pdfFile.setTenantName(longRentOrder.getTenantName());
         pdfFile.setRoom(longRentOrder.getRoom());
-        LocalDateTime begin=LocalDateTime.parse(longRentOrder.getCheckInDay(),DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        LocalDateTime end=LocalDateTime.parse(longRentOrder.getLeaveDay(),DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        int months=(int)Math.ceil((end.toLocalDate().toEpochDay()-begin.toLocalDate().toEpochDay())/30.0);
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate begin1=LocalDate.parse(longRentOrder.getCheckInDay(),df);
+        LocalDate end1=LocalDate.parse(longRentOrder.getLeaveDay(),df);
+        LocalDateTime begin=begin1.atStartOfDay();
+        LocalDateTime end=end1.atStartOfDay();
+        int months=(int)((end1.toEpochDay()-begin1.toEpochDay())/30.0);
         pdfFile.setMonths(months);
         pdfFile.setStartDay(begin);
         pdfFile.setDueDay(end);
         PdfFile contract=pdfContractService.pdfContractMaking(pdfFile);
-        pdfFileRepository.save(contract);
-        longRentOrder.setPdffile(contract);
+        PdfFile pdfFile1=pdfFileRepository.save(contract);
+        String url="/api/file/getGontract/"+pdfFile1.getId();
+        longRentOrder.setUrl(url);
+        longRentOrderRepository.save(longRentOrder);
         return contract.getContent().getData();
+    }
+    @GetMapping(value = "file/getGontract/{id}",produces = {MediaType.APPLICATION_PDF_VALUE})
+    @ResponseBody
+    public byte[] downloadPdf(@PathVariable String id){
+        byte[] data=null;
+        PdfFile pdfFile=pdfFileRepository.findById(id).get();
+        if(pdfFile!=null)
+            data=pdfFile.getContent().getData();
+        return data;
     }
 }
